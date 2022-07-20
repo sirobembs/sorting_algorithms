da8552d112ebbd2701de559336d4d5ead2440e5e
sirobembs <danielobembe1@gmail.com>
2022-07-19 18:14:28 -0700

    function that sorts an array of integers in ascending order using the Selection sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,4 +1,4 @@
-#include "sort.h"  
+#include "sort.h"
 
 /**
  * swap - Swaps two integer pointers
@@ -18,30 +18,30 @@ void swap(int *xp, int *yp)
 /**
  * selection_sort - sorts an array by holding the righmost value
  * and swaping with the least value in the rest of the array.
- * 
+ *
  * @array: Array of integer to be sorted
  * @size: size of @array
  */
 
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_index;
+	size_t i, j, min_index;
 
-    for(i = 0; i < (size - 1); i++)
-    {
-        min_index = i;
+	for (i = 0; i < (size - 1); i++)
+	{
+		min_index = i;
 
-        for(j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_index])
-            {
-                min_index = j;
-            }
-        }
-        if (min_index != i)
-        {
-            swap(&array[i], &array[min_index]);
-            print_array(array, size);
-        }
-    }
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+			{
+				min_index = j;
+			}
+		}
+		if (min_index != i)
+		{
+			swap(&array[i], &array[min_index]);
+			print_array(array, size);
+		}
+	}
 }
